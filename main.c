e437bff610222e1352a8d2a2f3f9488f01fcecc4
SuChenPUES <38059920+SuChenPUES@users.noreply.github.com>
2018-05-10 17:39:36 +0900

    minUnitに合わせる改造(2018/05/10)
    1. 電圧計算関数cal_voltage
    ・引数VBFLT_n_1(n-1ステップ目電圧)を削除し、代わりに出力量VBFLT_n(nステップ目電圧)を引数として追加した
    ・ステップ1の処理が1回目以降と整合しづらいと思い、分けて処理にした(main関数にてVB=0がVB≠0と分ける)
    2. minUnit
    ・main関数にてケースごとにテスト関数(define minUnit testing)を作成した
    ・main関数にてテスト実行関数(run minUnit testing)を作成した

--- main.c
+++ main.c
@@ -13,14 +13,14 @@
 #include "iodefine.h"
 #include "minunit.h"
 
-float cal_voltage(int n,float VB,float VBFLT_n_1);
-void main(void);
+float cal_voltage(int n, float VB, float VBFLT_n);
+int main(void);
 
 /*バッテリ電圧フィルター処理値計算関数*/
-float cal_voltage(int n,float VB,float VBFLT_n_1)
+float cal_voltage(int n, float VB, float VBFLT_n)
 {
-	float CFVBFLT = 0.5;/*バッテリ電圧フィルタリング係数*/
-        float VBFLT_n;/*バッテリ電圧フィルター処理値*/
+	float CFVBFLT = 0.5; //バッテリ電圧フィルタリング係数
+	float VBFLT_n_1; //(n-1)回目バッテリ電圧フィルター処理値
 	
 	
 	/*システム起動直後の値(=初回バッテリ電圧)*/
@@ -28,6 +28,10 @@ float cal_voltage(int n,float VB,float VBFLT_n_1)
 	{
 		VB = 0;
 		VBFLT_n_1 = 0;
+	}
+	else
+	{
+		VBFLT_n_1 = VBFLT_n;
 	}
 	
 	/*入力を目標範囲に修正*/
@@ -57,36 +61,127 @@ float cal_voltage(int n,float VB,float VBFLT_n_1)
 	
 }
 
-void main(void)
+
+int main(void)
 {
-	int num_case = 5;/*ケース数*/
-	int n = 10;/*ステップ数*/
-	int i;
-	int j;
+	int num_case = 5; //ケース数
+	int num_step = 10; //ステップ数
+	int i; //ステップ数カウンター
+	int j; //ケース数カウンター
 	
-	float VBFLT_n_1;/*(n-1)回目バッテリ電圧フィルター処理値*/
-	float VB[5] = {24.4,24.5,12.2,50,-10};/*各ケースのバッテリ電圧*/
-	float VBFLT[5][10];/*バッテリ電圧フィルター処理値配列[ケース][ステップ]*/
+	float VB[5] = {24.4, 24.5, 12.2, 50, -10}; //各ケースのバッテリ電圧
+	float VBFLT[5][10]; //バッテリ電圧フィルター処理値配列[ケース][ステップ]
 	
-	float test_case1[10] = {0, 12.2, 18.3, 21.35, 22.875, 23.6375, 24.01875, 24.209375, 24.3046875, 24.35234375}; /*テストケース1*/
-	
-	for (j = 0;j < num_case;j ++)
+	for (j = 0; j < num_case; j ++)
 	{	
-		for (i = 0; i < n;i ++)
+		/*0回目バッテリ電圧フィルター処理値算出*/
+		i == 0;
+		VBFLT[j][i] = cal_voltage(i, VB[j], VBFLT[j][i]);
+		
+		/*ステップj回目バッテリ電圧フィルター処理値順次に算出*/
+		for (i = 1; i < num_step; i ++)
 		{
-			VBFLT[j][i] = cal_voltage(i,VB[j],VBFLT_n_1);/*i回目バッテリ電圧フィルター処理値算出*/
-			VBFLT_n_1 = VBFLT[j][i];/*i回目バッテリ電圧フィルター処理値を保存して次回に(i-1)回目の値として使用*/
+			VBFLT[j][i] = cal_voltage(i, VB[j], VBFLT[j][i-1]);
 		}
 	}
 	
-	/*minUnit testing*/
+	/*define minUnit testing*/
+	float test_case1[10] = {0, 12.2, 18.3, 21.35, 22.875, 23.6375, 24.01875, 24.209375, 24.3046875, 24.35234375}; //テストケース1
+	float test_case2[10] = {0, 12.2, 18.3, 21.35, 22.875, 23.6375, 24.01875, 24.209375, 24.3046875, 24.35234375}; //テストケース2
+	float test_case3[10] = {0, 6.1, 9.15, 10.675, 11.4375, 11.81875, 12.009375, 12.1046875, 12.15234375, 12.17617188}; //テストケース3
+	float test_case4[10] = {0, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4}; //テストケース4
+	float test_case5[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //テストケース5
+	
 	static char *cal_voltage_case1()
 	{
-		for (i = 0;i < n;i ++)
+	        i = 0
+	        mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[1][i]) == test_case1[i]);
+		return 0;
+		
+		for (i = 1; i < num_step; i ++)
 		{
-		    mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT_n_1) == test_case1[i]);
-	
+		        mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[1][i-1]) == test_case1[i]);
+			return 0;
 		}
 	}
 
+	static char *cal_voltage_case2()
+	{
+	        i = 0
+                mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[2][i]) == test_case2[i]);
+		
+		for (i = 1; i < num_step; i ++)
+		{
+		        mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[2][i-1]) == test_case2[i]);
+			return 0;
+		}
+	}
+
+	static char *cal_voltage_case3()
+	{
+	       i = 0
+	       mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[3][i]) == test_case3[i]);
+	       return 0;
+		
+	       for (i = 1; i < num_step; i ++)
+	       {
+		        mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[3][i-1]) == test_case3[i]);
+			return 0;
+	       }
+	}
+	
+	static char *cal_voltage_case4()
+	{
+	       i = 0
+	       mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[4][i]) == test_case4[i]);
+	       return 0;
+		
+	       for (i = 1; i < num_step; i ++)
+	       {
+		       mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[4][i-1]) == test_case4[i]);
+		       return 0;
+	       }
+	}
+	
+	static char *cal_voltage_case5()
+	{
+	       i = 0
+	       mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[5][i]) == test_case5[i]);
+	       return 0;
+	
+	       for (i = 1; i < num_step; i ++)
+	       {
+		      mu_assert("error voltage value !", cal_voltage(i, VB[i], VBFLT[5][i-1]) == test_case5[i]);
+		      return 0;
+	       }
+	}
+	
+	static char * all_tests() //テストを実施する関数
+	{    
+	        mu_run_test(cal_voltage_case1);
+                mu_run_test(cal_voltage_case2);
+		mu_run_test(cal_voltage_case3);
+		mu_run_test(cal_voltage_case4);
+		mu_run_test(cal_voltage_case5);
+                return 0;
+	}
+
+	/*run minUnit testing*/
+	char s[255]={0};
+
+        int a=1;
+        char *result = all_tests();
+        if (result != 0) 
+	{
+	        sprintf(s, "%s\n",result);
+	}
+        else
+	{
+	        sprintf(s, "ALL OK! \n");
+	}
+	
+        /* printf("test num: %d\n",tests_run); */
+        return result != 0;
+
+	
 }
