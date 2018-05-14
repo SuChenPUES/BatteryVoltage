5ab446e673736491ce2db880ad806469a7cb93ce
SuChenPUES <38059920+SuChenPUES@users.noreply.github.com>
2018-05-14 11:00:50 +0900

    テスト関数の判定条件変更
    ・判定条件
    変更前：cal_voltage[i] == test_case[i]
    変更後：abs(cal_voltage[i] -  test_case[i])
     <= 1e-7
    浮動小数点を考慮し、判定条件を変更した。
    
    ・カウンター用変数iを各テスト関数にてそれぞれ宣言される

--- main.c
+++ main.c
@@ -19,10 +19,11 @@ int main(int argc, char **argv);
 /*バッテリ電圧フィルター処理値計算関数*/
 float cal_voltage(float VB)
 {
+	int i;
 	float CFVBFLT = 0.5; //バッテリ電圧フィルタリング係数
 	static float VBFLT_n_1 = 0; //(n-1)回目バッテリ電圧フィルター処理値
 	float VBFLT_n; //(n)回目バッテリ電圧フィルター処理値
-	
+
 	/*入力を目標範囲に修正*/
 	if (VB < 0)
 	{
@@ -56,16 +57,17 @@ float cal_voltage(float VB)
 /*define minUnit testing*/
 int tests_run = 5; //テストケース数
 int num_step = 10; //ステップ数
-int i;
 
 static char *cal_voltage_case1()
 {
 	float VB1[10] = {0, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4}; //ケースのバッテリ電圧入力
 	float test_case1[10] = {0, 12.2, 18.3, 21.35, 22.875, 23.6375, 24.01875, 24.209375, 24.3046875, 24.35234375}; //ケースのバッテリ電圧フィルター処理値の期待値
 	
+	int i;
+	
 	for (i = 0; i < num_step; i ++)
 	{
-		mu_assert("error voltage value !", cal_voltage(VB1[i]) == test_case1[i]);
+		mu_assert("error voltage value !", abs(cal_voltage(VB1[i]) - test_case1[i]) <= (1e-7));
 	}
 	
 	return 0;
@@ -76,9 +78,11 @@ static char *cal_voltage_case2()
 	float VB2[10] = {0, 24.5, 24.5, 24.5, 24.5, 24.5, 24.5, 24.5, 24.5, 24.5};
 	float test_case2[10] = {0, 12.2, 18.3, 21.35, 22.875, 23.6375, 24.01875, 24.209375, 24.3046875, 24.35234375};
 	
+	int i;
+	
 	for (i = 0; i < num_step; i ++)
 	{
-		mu_assert("error voltage value !", cal_voltage(VB2[i]) == test_case2[i]);
+		mu_assert("error voltage value !", abs(cal_voltage(VB2[i]) - test_case2[i]) <= (1e-7));
 	}
 	
 	return 0;
@@ -89,9 +93,11 @@ static char *cal_voltage_case3()
 	float VB3[10] = {0, 12.2, 12.2, 12.2, 12.2, 12.2, 12.2, 12.2, 12.2, 12.2};
 	float test_case3[10] = {0, 6.1, 9.15, 10.675, 11.4375, 11.81875, 12.009375, 12.1046875, 12.15234375, 12.17617188};
 	
+	int i;
+	
 	for (i = 0; i < num_step; i ++)
 	{
-		mu_assert("error voltage value !", cal_voltage(VB3[i]) == test_case3[i]);
+		mu_assert("error voltage value !", abs(cal_voltage(VB3[i]) - test_case3[i]) <= (1e-7));
 	}
 	
 	return 0;
@@ -102,9 +108,11 @@ static char *cal_voltage_case4()
 	float VB4[10] = {0, 50, 50, 50, 50, 50, 50, 50, 50, 50};
 	float test_case4[10] = {0, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4, 24.4};
 	
+	int i;
+	
 	for (i = 0; i < num_step; i ++)
 	{
-		mu_assert("error voltage value !", cal_voltage(VB4[i]) == test_case4[i]);
+		mu_assert("error voltage value !", abs(cal_voltage(VB4[i]) - test_case4[i]) <= (1e-7));
 	}
 	
 	return 0;
@@ -115,9 +123,11 @@ static char *cal_voltage_case5()
 	float VB5[10] = {0, -10, -10, -10, -10, -10, -10, -10, -10, -10};
 	float test_case5[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 	
+	int i;
+	
 	for (i = 0; i < num_step; i ++)
 	{
-		mu_assert("error voltage value !", cal_voltage(VB5[i]) == test_case5[i]);
+		mu_assert("error voltage value !", abs(cal_voltage(VB5[i]) - test_case5[i]) <= (1e-7));
 	}
 	
 	return 0;
